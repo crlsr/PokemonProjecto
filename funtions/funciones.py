ec986f5308bc22e56aeca26faa3539c448de9ee5
crlsr <r.carlos@correo.unimet.edu.ve>
2024-03-26 21:46:02 -0400

    Create funciones.py

---
+++ funtions/funciones.py
@@ -0,0 +1,31 @@
+'''
+Este modulo es dedicdo a la creación de todas las funciones creadas para darle el funcionamiento a nuestro programa
+En este apartados abran aspectos como validaciones, etc...
+'''
+#Importaciones de librerias necesarias para la creación de funciones
+from random import randint
+
+
+#Función para validar entradas númericas definidas en un rango númerico
+def validation(val, minval, maxval): 
+    while val<minval or val>maxval:
+        try:
+            val = int(input (f'Error, recuerde que los valores que puede elegir van desde {minval} hasta {maxval}: '))
+        except ValueError:
+            print('Valor incorrecto, solo se admiten números')
+    return val
+
+
+#Función para validar valores iniciales (restringiendo el tipo de dato ue puede llegar a ser)
+def int_validatión(string):
+    while True:
+        try: 
+            valor = int(input(string))
+            return valor 
+        except ValueError:
+            print('Error, valor incorrecto, solo se admiten números.')
+            
+
+
+#Combinación que permite verificar un valor y a la vez validarlo entre un conjunto numerico definido.
+x = validation(int_validatión(''), 1, 2) 
