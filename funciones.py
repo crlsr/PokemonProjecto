35c76bad2596db1e04d491c68eaf0113de5c092f
crlsr <r.carlos@correo.unimet.edu.ve>
2024-03-26 21:46:07 -0400

    Delete funciones.py

--- funciones.py
+++
@@ -1,4 +0,0 @@
-'''
-Este modulo es dedicdo a la creación de todas las funciones creadas para darle el funcionamiento a nuestro programa
-En este apartados abran aspectos como la definición de ataques, las batallas, etc...
-'''
