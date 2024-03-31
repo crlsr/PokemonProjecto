99ae7b498ccc6e8782a23a35b592a17a80d9e412
diegofernandezv <156604267+diegofernandezv@users.noreply.github.com>
2024-03-31 01:05:05 -0400

    Add files via upload

---
+++ DIALOGOS.py
@@ -0,0 +1,57 @@
+#ARCHIVO DE DIALOGOS (COPY & PASTE EN EL PROGRAMA)
+
+#PUEBLO NARANJA
+
+print("En una tierra bastante lejana, está la tierra de textemon. Un mundo donde vagaban monstruos basados\n"
+      "​​en texto, un mundo donde vagaban monstruos basados ​​en texto\n"
+      "\n"
+      "Esta idea y todos los nombres son de la empresa Nintendo y Game Freak.\n"
+      "\n"
+      "Pokémon es una marca registrada de Nintendo. Este proyecto no está afiliado ni respaldado por Nintendo.")
+
+
+#PUEBLO NARANJA
+
+print("¡Bienvenidos al pueblo inicial, Pueblo Naranja! Este pueblo costero en Kanto es conocido por su puerto y los cruceros como el S.S. Anne. \n"
+      "Con una historia que se remonta a tribus indígenas, se ha convertido en un centro de comercio y turismo, enriqueciendo su cultura con influencias globales. \n"
+      "Con calles pintorescas y una comunidad diversa, es un destino único en la región de Kanto.")
+
+#PUEBLO AZUL
+
+print("¡Bienvenidos al Pueblo Azul! Esta encantadora ciudad es uno de los primeros destinos que exploran los entrenadores Pokémon en su viaje por la región de Kanto. \n"
+      "Con su distintivo aspecto acuático y su famoso gimnasio Pokémon, Ciudad Azuliza se ha convertido en un lugar emblemático dentro del mundo Pokémon.")
+
+#PUEBLO ROJO
+
+print("¡Bienvenidos al Pueblo Rojo! Este pueblo es famoso por su Torre Pokémon, también conocida como la Torre Fantasma. \n"
+      "Se trata de un antiguo cementerio de Pokémon donde se cree que habitan los espíritus de los Pokémon fallecidos. \n"
+      "Los entrenadores Pokémon visitan esta torre en su viaje para investigar fenómenos paranormales y enfrentarse a los Pokémon Fantasma que la habitan.")
+
+#PUEBLO NEGRO
+
+print("¡Bienvenidos al Pueblo Negro! Este bullicioso centro urbano en la región de Kanto es famoso por su gimnasio Pokémon especializado en Pokémon psíquicos, liderado por Sabrina. \n"
+      "Además, es el hogar de la Corporación Silph, un importante centro de producción de artículos para entrenadores Pokémon. \n"
+      "¡Disfruten su estadía en esta vibrante ciudad!")
+
+#PUEBLO FUXIA
+
+print("¡Bienvenidos al Pueblo Fucsia! Este encantador pueblo, ubicado al sur de la región de\n"
+      "Kanto en el mundo Pokémon, es conocido por su gimnasio especializado en Pokémon de\n"
+      "tipo veneno, liderado por Koga. Además, los entrenadores pueden disfrutar del emocionante\n"
+      "Safari Pokémon y visitar el Centro de Crianza Pokémon para criar a sus compañeros.\n"
+      "Pueblo Fucsia ofrece una experiencia única en el viaje de cualquier entrenador Pokémon.\n"
+      "¡Disfruten su estadía!")
+
+#PUEBLO PLATEADO
+
+print("¡Bienvenidos al Pueblo Plateado! Este pintoresco pueblo se encuentra al noroeste \n"
+      "de la región de Kanto y es conocido por su proximidad al imponente Monte Plateado \n"
+      "Esta majestuosa montaña, con sus paisajes rocosos y montañosos, evoca tonos oscuros \n"
+      "y grises, aunque su nombre no se relaciona directamente con el color negro. Aunque \n"
+      "en los juegos principales de Pokémon no existe un pueblo específico conocido como \n"
+      "Pueblo Negro, la idea de un lugar asociado con el color oscuro agrega un toque único \n"
+      "y misterioso a la región de Kanto en tu imaginación.")
+
+
+
+
