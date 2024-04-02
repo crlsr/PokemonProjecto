6a12d049c29c2d7b4a38e627669153d8a4e1e828
crlsr <r.carlos@correo.unimet.edu.ve>
2024-04-01 20:26:18 -0400

    commit partner

--- Class/partner.py
+++ Class/partner.py
@@ -1,7 +1,20 @@
+#Usamos script para evitar excepciones de importación circular
+import sys, os
+actual_dir = os.path.dirname(os.path.abspath(__file__))
+principal_dir = os.path.join(actual_dir, '..')
+sys.path.append(principal_dir)
+
+#Importamos trainer para crear pokecompañero como herencia de trainer
+from Class.pokeclases import Trainer
 #Clase dedicada a la creación del compañero humano
-class Pokecompañero:
-    def __init__(self, nombre, pokemons = None):
-        self.nombre = nombre
+class Pokecompañero(Trainer):
+    def __init__(self, nombre, pokemons = None, objetos = None, ubicacion = None):
         if pokemons is None:
             pokemons = []
-        self.pokemons = pokemons
+        if objetos is None:
+            objetos = []
+        if ubicacion is None:
+            ubicacion = ''
+        super().__init__(nombre, pokemons, objetos, ubicacion)
+        
+        
